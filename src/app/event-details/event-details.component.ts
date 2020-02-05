6222f095bb6bb15115f8f862044faae78b06e7ea
halityurtsever <halityurtsever@gmail.com>
2020-02-05 22:20:23 +0100

    using ActivatedRoute service to get ids from url

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../events/shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  templateUrl: './event-details.component.html'
+  templateUrl: './event-details.component.html',
+  styles: [`
+  .container { padding: 20px; }
+  .event-image { height: 150px; }
+  `]
 })
 export class EventDetailsComponent implements OnInit {
   event: any;
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
   }
 
 }
