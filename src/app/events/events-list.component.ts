9cfe529f58a777f54cab61349349376d841eceb1
halityurtsever <halityurtsever@gmail.com>
2020-02-05 20:33:33 +0100

    implement toastr

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 
+declare let toastr;
+
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html'
@@ -13,5 +15,9 @@ export class EventsListComponent implements OnInit {
 
   ngOnInit(): void {
     this.events = this.eventService.getEvents();
+  }
+
+  onThumbnailClick(eventName){
+    toastr.success(eventName);
   }
 }
