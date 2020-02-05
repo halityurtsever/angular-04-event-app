a0c629e0f2b5f817854e70dcba694d0013fea373
halityurtsever <halityurtsever@gmail.com>
2020-02-05 18:56:58 +0100

    ngClass statement

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,8 +3,11 @@ import { Component, Input, } from '@angular/core';
 @Component({
   selector: 'app-event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
-  styles:[`
-  .thumbnail{min-height:210px;}
+  styles: [`
+  .thumbnail { min-height:210px; }
+  .green { color:#ABEF00; }
+  .red { color:#FF0400; }
+  .yellow { color:#F7F052; }
   `]
 })
 
@@ -12,4 +15,13 @@ export class EventThumbnailComponent {
 
   @Input() eventInput: any;
 
+  getTimeClass() {
+    if (this.eventInput.time === '8:00 am') {
+      return ['green'];
+    } else if (this.eventInput.time === '10:00 am') {
+      return ['red'];
+    } else {
+      return ['yellow'];
+    }
+  }
 }
