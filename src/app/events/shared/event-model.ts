bcfc73b4ae085475b779d628c2b1f01bdf5e1fce
halityurtsever <halityurtsever@gmail.com>
2020-02-06 21:41:34 +0100

    defining event and session types

---
+++ src/app/events/shared/event-model.ts
@@ -0,0 +1,17 @@
+import { ISessionModel } from './session-model';
+
+export interface IEventModel {
+    id: number,
+    name: string,
+    date: Date,
+    time: string,
+    price: number,
+    imageUrl: string,
+    location?: {
+        address: string,
+        city: string,
+        country: string
+    },
+    onlineUrl?: string,
+    sessions: ISessionModel[]
+}
---
+++ src/app/events/shared/session-model.ts
@@ -0,0 +1,9 @@
+export interface ISessionModel {
+    id: number,
+    nme: string,
+    presenter: string,
+    duration: number,
+    level: string,
+    abstract: string,
+    voters: string[]
+}
