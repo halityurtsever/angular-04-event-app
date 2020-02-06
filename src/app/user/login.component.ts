b57d5387ddb0d46bc9b5c8ab64709a7a771d65b1
halityurtsever <halityurtsever@gmail.com>
2020-02-07 00:44:49 +0100

    adding some form validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,12 +3,16 @@ import { AuthenticationService } from './authentication.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+  em { float: right; color: #FF0400;}
+  `]
 })
 
 export class LoginComponent implements OnInit {
   userName: string;
   password: string;
+  mouseoverLogin: boolean;
 
   constructor(private authService: AuthenticationService, private router: Router) { }
 
@@ -17,7 +21,7 @@ export class LoginComponent implements OnInit {
     this.router.navigate(['events']);
   }
 
-  cancel(){
+  cancel() {
     this.router.navigate(['events']);
   }
 
