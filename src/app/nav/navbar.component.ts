9b32b0add6b65492fefe3b51d83c7740743b661d
halityurtsever <halityurtsever@gmail.com>
2020-02-06 13:52:03 +0100

    styling active links

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styles: [`
   .nav.navbar-nav{font-size:15px;}
   @media (max-width:1200px) {#searchForm {display:none}}
+  li > a.active { color: #D7983D; }
   `]
 })
 export class NavbarComponent {
